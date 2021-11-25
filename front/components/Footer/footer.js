19d3afb0b6fd199bba3691681f5db10ef3eb5b86
Matus <matus.sasik@gmail.com>
2021-11-25 14:22:48 +0100

    Pridaný footer

--- front/components/Footer/footer.js
+++ front/components/Footer/footer.js
@@ -3,13 +3,12 @@ import React from 'react';
 
 const Footer = () => {
     return (
-
-        <footer className="container">
-            <p className="float-end"><a href="#">Back to top</a></p>
-            <p>© Copyright 2021 PWTkurier· <a href="#">Privacy</a> · <a href="#">Terms</a></p>
+        <footer className="py-3 my-4">
+            <ul className="nav justify-content-center border-bottom pb-3 mb-3">
+                <li className="nav-item"><a href="#" className="nav-link px-2 text-muted">Back to top</a></li>
+            </ul>
+            <p className="text-center text-muted">© Copyright 2021 PWTkurier</p>
         </footer>
-
-
     );
 };
 
